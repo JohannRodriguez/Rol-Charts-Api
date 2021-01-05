d14fdf26d417b9804beebf882de9bd77b503dcfd
Lilith <lilith_niniane@outlook.com>
2021-01-05 17:43:43 -0600

    Create user model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+  PASSWORD_FORMAT = /\A
+    (?=.*\d)
+    (?=.*[a-z])
+    (?=.*[A-Z])
+    (?=.*[[:^alnum:]])
+  /x
+
+  validates :email, presence: true, uniqueness: true, email: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 28}, format: { with: PASSWORD_FORMAT }
+  validates :username, presence: true, length: { minimum: 5, maximum: 16}, format: { with:  /\A[a-zA-Z0-9 ]+\z/, message: 'Only letters and numbers' }
+end
