b52b733306bb7ec536d360f6549a73ce81497f8c
Lilith <lilith_niniane@outlook.com>
2021-01-08 02:17:17 -0600

    App.js now store cookies. Create register page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,24 +1,48 @@
-import React, { useEffect } from 'react';
-import { default as request } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { default as callApi } from 'axios';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Login from './Login';
+import Register from './Register';
 
 const App = () => {
+  const [log, setLog] = useState(
+    {
+      logStatus: 'NOT_LOGGED_IN',
+      user: null,
+    }
+  );
+
   useEffect(() => {
-    request
+    callApi
       .get('/api/v1/log_status', { withCredentials: true })
-      .then(response => console.log(response))
+      .then(response => {
+        if (response.data.logStatus && log.logStatus === 'NOT_LOGGED_IN') {
+          setLog({
+            ...log,
+            logStatus: 'NOT_LOGGED_IN',
+            user: response.data.user,
+          });
+        }
+      })
       .catch(error => console.log(error));
   });
 
   return (
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
+      <Route exact path={'/'} render={props => (
+        <Home {...props} user={log.logStatus} // handleLogin={handleLogin}handleLogout={handleLogout}
+        />
+      )}/>
+      <Route exact path='/login' render={props => (
+        <Login {...props} />
+      )}/>
+      <Route exact path='/register' render={props => (
+        <Register {...props} />
+      )}/>
     </Switch>
   );
 };
 
-export default App
+export default App;
---
+++ app/javascript/components/Register.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { default as callApi } from 'axios';
+
+const Register = () => {
+  const [field, setField] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+    registrationErrors: '',
+  });
+
+  const handleSubmit = event => {
+    const { username, email, password, password_confirmation } = field;
+    callApi
+      .post(
+        '/api/v1/users',
+        {
+          user: {
+            username: username,
+            email: email,
+            password: password,
+            password_confirmation: password_confirmation,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log('registration error', error);
+      })
+    event.preventDefault();
+  };
+
+  const handleChange = event => {
+    setField({
+      ...field,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="User name"
+          value={field.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={field.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={field.password}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password_confirmation"
+          placeholder="Confirm Password"
+          value={field.password_confirmation}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
