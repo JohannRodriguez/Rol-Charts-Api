f9e2cd320ff34a6d3649b466894e8f3f77d2a1de
Lilith <themrblacksapce@hotmail.com>
2021-02-02 19:33:03 -0700

    fix database production address and correct permited url cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_rol_charts_app', domain: 'https://rol-charts.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store, key: '_rol_page_app', domain: 'rol-page.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_rol_charts_app'
+  Rails.application.config.session_store :cookie_store, key: '_rol_page_app'
 end
